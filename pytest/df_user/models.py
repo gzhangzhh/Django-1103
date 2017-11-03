aa3d31e5b4eb5432b3c6fc223a440e3f7d5efc6b
xwp <xwp_fullstack@163.com>
2017-11-03 23:15:36 +0800

    用户中心初步完成

--- pytest/df_user/models.py
+++ pytest/df_user/models.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from django.db import models
 
 # Create your models here.
@@ -5,7 +6,8 @@ class UserInfo(models.Model):
     uname = models.CharField(max_length=20)
     upwd = models.CharField(max_length=40)
     uemail = models.CharField(max_length=30)
-    ushou = models.CharField(max_length=20)
-    uaddress = models.CharField(max_length=100)
-    uyoubian = models.CharField(max_length=6)
-    uphone = models.CharField(max_length=11)
+    ushou = models.CharField(max_length=20,default='')
+    uaddress = models.CharField(max_length=100,default='')
+    uyoubian = models.CharField(max_length=6,default='')
+    uphone = models.CharField(max_length=11,default='')
+    #default,blank 是python层面的约束，不需要重新迁移
--- pytest/pytest/urls.py
+++ pytest/pytest/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^freshfruit',include('freshfruit.urls')),
+    url(r'^user/',include('df_user.urls')),
 ]
